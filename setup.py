3c76d7ecd575659d685e0f79e49aebff2c0fa2f3
Leon Vack <LVack@vi-solutions.de>
2015-03-04 14:41:04 +0100

    setup.py, dns lookup
    node init and connect_to_node now make dns lookups
    setup.py and __init__.py added
    src folder is now called p2p_network
    new exampel folder containing 1 example of a chat/filesending node

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='p2p_network',
+    version='0.1.0',
+    packages=['p2p_network', 'p2p_network.utils'],
+    url='https://github.com/LogicalOverflow/p2p_network',
+    license='',
+    author='LVack',
+    author_email='',
+    description='tcp socket peer to peer network'
+)
